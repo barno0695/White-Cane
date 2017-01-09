48c6d98aa153b1e516b803b90432bd46886a1ea5
Barnopriyo Barua <barno0695@gmail.com>
2017-01-09 16:42:38 +0530

    IP change inside app, not using inbuilt camera app now. TODO: take picture using voice command loading screen

--- WhiteCane/app/src/main/java/com/example/barno/aiapp/MainActivity.java
+++ WhiteCane/app/src/main/java/com/example/barno/aiapp/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.barno.aiapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +38,9 @@ import org.apache.http.entity.mime.content.ContentBody;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,8 +48,11 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 
+import static android.R.attr.angle;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,28 +60,166 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     private static final int SELECT_PICTURE = 1889;
     private static final int REQ_CODE_SPEECH_INPUT = 1890;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1891;
     private ImageView imageView;
     private TextView txtSpeechInput;
     TextToSpeech textToSpeech;
     EditText captionText;
     Button listenButton;
     Button commandButton;
+    EditText ipText;
+    Button showIP;
+    Button changeIP;
+    LinearLayout ipLayout;
+    CameraPreview mPreview;
+    Camera mCamera;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+    final Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            System.out.println(data.length);
+            Bitmap photo;
+            photo= BitmapFactory.decodeByteArray(data, 0, data.length);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            photo = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight(), matrix, true);
+            imageView.setImageBitmap(photo);
+            String filename = "filename.png";
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+            ContentBody contentPart = new ByteArrayBody(bos.toByteArray(), filename);
+
+            final MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+            reqEntity.addPart("file", contentPart);
+
+            Thread thread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try  {
+
+                        String response = multipost("http://" + ipText.getText() + ":5000/upload", reqEntity);
+
+                        System.out.println("**********************************" + response);
+                        captionText.setText(response);
+                        speak();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+
+            thread.start();
+            try { thread.join(); } catch (InterruptedException e) { e.printStackTrace(); }
+
+            mCamera.stopPreview();
+            mCamera.startPreview();
+
+
+        }
+    };
+
+    public void checkPermisions() {
+        if ( ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},
+                    1);
+
+        }
+
+        if ( ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    1);
+
+        }
+    }
+
+//    public void capturePic()
+//    {
+////        mCamera = Camera.open();
+//        mCamera.setDisplayOrientation(90);
+//
+//        // Create our Preview view and set it as the content of our activity.
+//        mPreview = new CameraPreview(this, mCamera);
+//        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+//        preview.addView(mPreview);
+//
+//        mCamera.takePicture(null, null, pictureCallback);
+//
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermisions();
         setContentView(R.layout.activity_main);
         this.imageView = (ImageView)this.findViewById(R.id.imageHolder);
         Button photoButton = (Button) this.findViewById(R.id.cameraButton);
         Button galleryButton = (Button) this.findViewById(R.id.galleryButton);
         this.txtSpeechInput = (TextView) this.findViewById(R.id.txtSpeechInput);
+        this.ipText = (EditText) this.findViewById(R.id.ipText);
+        this.showIP = (Button) this.findViewById(R.id.showIP);
+        this.changeIP = (Button) this.findViewById(R.id.changeIP);
+        this.ipLayout = (LinearLayout) this.findViewById(R.id.ipLayout);
+        this.mCamera = Camera.open();
+        this.mCamera.setDisplayOrientation(90);
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+        preview.addView(mPreview);
+
+
+
 
         photoButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+
+                mCamera.takePicture(null, null, pictureCallback);
+
+            }
+        });
+
+        showIP.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ipLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        changeIP.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (!ipText.getText().equals("")) {
+                    ipLayout.setVisibility(View.GONE);
+                }
             }
         });
 
@@ -212,39 +367,7 @@ public class MainActivity extends AppCompatActivity {
     Bitmap photo;
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
-            String filename = "filename.png";
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            photo.compress(Bitmap.CompressFormat.JPEG, 100, bos);
-            ContentBody contentPart = new ByteArrayBody(bos.toByteArray(), filename);
 
-            final MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-            reqEntity.addPart("file", contentPart);
-
-            Thread thread = new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    try  {
-
-                         String response = multipost("http://10.109.25.35:5000/upload", reqEntity);
-
-                        System.out.println("**********************************" + response);
-                        captionText.setText(response);
-                        speak();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-
-            thread.start();
-            try { thread.join(); } catch (InterruptedException e) { e.printStackTrace(); }
-            speak();
-        }
         if (requestCode == SELECT_PICTURE  && resultCode == Activity.RESULT_OK) {
             // Get the url from data
             Uri selectedImageUri = data.getData();
@@ -275,7 +398,7 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         try  {
 
-                            String response = multipost("http://10.109.25.35:5000/upload", reqEntity);
+                            String response = multipost("http://" + ipText.getText() + ":5000/upload", reqEntity);
 
                             System.out.println("**********************************" + response);
                             captionText.setText(response);
@@ -309,8 +432,10 @@ public class MainActivity extends AppCompatActivity {
 
                 else if(command.toLowerCase().contains("camera") || command.toLowerCase().contains("front") || command.toLowerCase().contains("see"))
                 {
-                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+//                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    mCamera.takePicture(null, null, pictureCallback);
+//                    capturePic();
                 }
 
                 else if(command.toLowerCase().contains("gallery"))
